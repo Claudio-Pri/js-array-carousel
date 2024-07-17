16cb731e04498285a57f74d9524e3643c61065c9
Claudio-Pri <priv.claudio@gmail.com>
2024-07-17 16:46:45 +0200

    primo step milestone 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,8 @@
 console.log('js collegato');
 
 // `
-// MILESTONE 2
-// Adesso rimuoviamo tutto il markup statico
-//e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito
-//e un semplice ciclo for che concatena un template literal.
 
+//dichiarazione array url immagini
 let myImage = ["img/fantasy-1.jpeg", "img/fantasy-2.jpeg", "img/fantasy-3.jpeg", "img/fantasy-4.jpeg", "img/fantasy-5.jpeg",];
 
 const myContainer = document.querySelector('.img-container');
@@ -14,15 +11,13 @@ const myContainer = document.querySelector('.img-container');
 for (i = 0; i < myImage.length; i++) {
     console.log(myImage[i]);
 
+    myContainer.innerHTML += `
+    <div class="item">
+
+    <img src="${myImage[i]}" class="image" alt="image">
+
+    </div>
+    `;
 }
 
-myContainer.innerHTML += `
-<div class="item">
 
-  <img src="${myImage[0]}" class="image" alt="image">
-
-</div>
-`;
-
-//perchè non vede la regola css di .image?
-console.log('mycontainer', myContainer, typeof myContainer);
