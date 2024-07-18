c48d15c567a48a77f1d8699cc0c37c0d0eb2545b
Claudio-Pri <priv.claudio@gmail.com>
2024-07-18 17:22:36 +0200

    JS commentato/ modifiche html css

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,39 @@
-console.log('js collegato');
 
 // `
 
 //dichiarazione array url immagini
-let myImage = ["img/fantasy-1.jpeg", "img/fantasy-2.jpeg", "img/fantasy-3.jpeg", "img/fantasy-4.jpeg", "img/fantasy-5.jpeg",];
+// let myImage = ["img/fantasy-1.jpeg", "img/fantasy-2.jpeg", "img/fantasy-3.jpeg", "img/fantasy-4.jpeg", "img/fantasy-5.jpeg",];
 
-const myContainer = document.querySelector('.img-container');
+// const myContainer = document.querySelector('.img-container');
+
+// let j = 0;
+// const nextBtn = document.getElementById('next-btn');
+// const prevBtn = document.getElementById('prev-btn');
+
+// nextBtn.addEventListener('click',
+//     function () {
+//         j++
+//     }
+// )
+
+// for (i = 0; i < myImage.length; i++) {
+//     console.log(myImage[i]);
+//     myContainer.innerHTML += `
+//     <div class="item">
+
+//         <img src="${myImage[j]}" class="image ${(j == i) ? 'd-block' : 'd-none'}" alt="image">
+
+//     </div>
+// `;
+//inserire event listener su buttons per j++ e j--
+// }
+
+//====== CORREZIONE ========
 
 
-for (i = 0; i < myImage.length; i++) {
-    console.log(myImage[i]);
 
-    myContainer.innerHTML += `
-    <div class="item">
 
-    <img src="${myImage[i]}" class="image" alt="image">
 
-    </div>
-    `;
-}
+
 
 
